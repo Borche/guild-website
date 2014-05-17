71862e9dd1525521dc4d4944df53dc65fa5b54f7
Andreas <borjesson.andreas@gmail.com>
2014-05-17 15:01:52 +0200

    Delete views.py

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from news.models import Article
-
-# Create your views here.
-def news(request):
-	articles = Article.objects.all().order_by('-pub_date')
-	return render(request, 'news/news.html', { 'articles': articles })
